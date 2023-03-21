a6509031daf5dc013840b0b094f8461498acacb1
Sergio Martín <sermmor@outlook.com>
2023-03-21 17:45:51 +0100

    Finished notification interface.

--- frontend/src/view/molecules/LabelAndDateTimeTextField/LabelAndDateTimeTextField.tsx
+++ frontend/src/view/molecules/LabelAndDateTimeTextField/LabelAndDateTimeTextField.tsx
@@ -1,8 +1,17 @@
 import { Box, TextField } from "@mui/material";
 import React from "react";
 
+const parseShortDateToLongDate = (date: string): string => {
+  // TODO: From '2023-03-22T11:02' to '2023-03-22T11:02:02.070Z'
+  return `${date}:00.000Z`;
+}
+
+const parseLongDateToShortDate = (date: string): string => {
+  // TODO: From '2023-03-22T11:02:02.070Z' to '2023-03-22T11:02'
+  return date.split(':').slice(0, 2).join(':');
+}
+
 export const LabelAndDateTimeTextField = ({text, onChange}: {text: string, onChange: (newText: string) => void}) => {
-  //
   const [isInEditMode, setEditMode] = React.useState<boolean>(false);
   const [textEditing, setTextEditing] = React.useState<string>(text);
 
@@ -16,17 +25,18 @@ export const LabelAndDateTimeTextField = ({text, onChange}: {text: string, onCha
       <Box sx={{width:'100%', display: 'flex', flexDirection: {xs: 'column', sm:'row'}, gap: '0.25rem', alignItems: 'center'}}>
         <TextField
           variant="standard"
-          value={textEditing}
+          type='datetime-local'
+          value={parseLongDateToShortDate(textEditing)}
           sx={{minWidth: {xs: '15.5rem', sm: '5rem', md: '5rem'}, width:'100%'}}
           onChange={evt => setTextEditing(evt.target.value)}
-          onBlur={() => setText(textEditing)} 
-          onKeyDown={(evt) => evt.key === 'Enter' || evt.key === 'Escape' ? setText(textEditing) : undefined}
+          onBlur={() => setText(parseShortDateToLongDate(textEditing))} 
+          onKeyDown={(evt) => evt.key === 'Enter' || evt.key === 'Escape' ? setText(parseShortDateToLongDate(textEditing)) : undefined}
           autoFocus={isInEditMode}
         />
       </Box>
     :
       <Box sx={{width:'100%', cursor: 'pointer', color: '#1976d2'}} onClick={() => setEditMode(true)}>
-        {textEditing}
+        {parseLongDateToShortDate(textEditing)}
       </Box>
   }</>;
 }
