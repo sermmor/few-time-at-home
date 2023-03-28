ef8e6a76be966e8017ddf32c639f57c076c87a12
Sergio Martín <sermmor@outlook.com>
2023-03-29 00:30:25 +0200

    Improved edit folder.

--- frontend/src/view/molecules/LabelAndTextFieldWithFolder/LabelAndTextFieldWithFolder.tsx
+++ frontend/src/view/molecules/LabelAndTextFieldWithFolder/LabelAndTextFieldWithFolder.tsx
@@ -7,12 +7,17 @@ export const LabelAndTextFieldWithFolder = ({text, onChange, setOpenFolder}: {
   onChange: (newText: string) => void,
   setOpenFolder: (label: string) => void}
 ) => {
+  const textSplited = text.split('/');
+  const path = textSplited.slice(0, textSplited.length - 1).join('/');
   const [isInEditMode, setEditMode] = React.useState<boolean>(false);
-  const [textEditing, setTextEditing] = React.useState<string>(text);
+  const [textToShow, setTextToShow] = React.useState<string>(text);
+  const [textToEdit, setTextToEdit] = React.useState<string>(textSplited[textSplited.length - 1]);
 
   const setText = (newText: string) => {
+    const textOnChange = `/${path}/${newText}`.split('//').join('/');
+    setTextToShow(textOnChange);
     setEditMode(false);
-    if (onChange) onChange(newText);
+    if (onChange) onChange(textOnChange);
   }
 
   return <> {
@@ -20,20 +25,20 @@ export const LabelAndTextFieldWithFolder = ({text, onChange, setOpenFolder}: {
       <Box sx={{width:'100%', display: 'flex', flexDirection: {xs: 'column', sm:'row'}, gap: '0.25rem', alignItems: 'center'}}>
         <TextField
           variant="standard"
-          value={textEditing}
+          value={textToEdit}
           sx={{minWidth: {xs: '15.5rem', sm: '5rem', md: '5rem'}, width:'100%'}}
-          onChange={evt => setTextEditing(evt.target.value)}
-          onBlur={() => setText(textEditing)} 
-          onKeyDown={(evt) => evt.key === 'Enter' || evt.key === 'Escape' ? setText(textEditing) : undefined}
+          onChange={evt => setTextToEdit(evt.target.value)}
+          onBlur={() => setText(textToEdit)} 
+          onKeyDown={(evt) => evt.key === 'Enter' || evt.key === 'Escape' ? setText(textToEdit) : undefined}
           autoFocus={isInEditMode}
         />
       </Box>
     :
       <Box sx={{width:'100%', display: 'flex', flexDirection: {xs: 'column', sm:'row'}, alignItems: 'center'}}>
         <Box sx={{ cursor: 'pointer', color: '#1976d2' }} onClick={() => setEditMode(true)}>
-          {textEditing}
+          {textToShow}
         </Box>
-        <Box sx={{ cursor: 'pointer', marginLeft: {xs: 'none', sm:'auto'}}} onClick={() => setOpenFolder(textEditing)}>
+        <Box sx={{ cursor: 'pointer', marginLeft: {xs: 'none', sm:'auto'}}} onClick={() => setOpenFolder(textToShow)}>
           <FolderIcon />
         </Box>
       </Box>
