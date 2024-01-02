077ce404c40f1fb9f8b31cc0aaebc849417a3eec
Sergio Martín <sermmor@outlook.com>
2024-01-02 10:22:02 +0100

    Added try catch for unread files.

--- backend/src/API/cloud.service.ts
+++ backend/src/API/cloud.service.ts
@@ -77,38 +77,43 @@ export class CloudService {
       files.forEach(filePath => {
         stat(`${path}/${filePath}`, (err, stat) => {
           if (!stat || !stat.isDirectory) console.log(`> Crash? : ${path}/${filePath}`);
-          if (stat && stat.isDirectory) {
-            if (stat.isDirectory()) {
-              directories.push(filePath);
-            } else {
-              allItemsAlreadyCollected.push({
-                name: filePath,
-                path: `${path}/${filePath}`,
-                driveName: drive.name,
-              });
-            }
-            numberFilesLeft--;
-  
-            if (numberFilesLeft === 0) {
-              if (directories.length === 0) {
-                resolve(allItemsAlreadyCollected);
+          try {
+            if (stat && stat.isDirectory) {
+              if (stat.isDirectory()) {
+                directories.push(filePath);
               } else {
-                let numberDirectoriesLeft = directories.length;
-                let newItems: CloudItem[] = [];
-                directories.forEach(directory => {
-                  setTimeout(() => {
-                    this.updateDriveIndex(drive, `${path}/${directory}`, allItemsAlreadyCollected).then(items => {
-                      newItems = items.filter(i => allItemsAlreadyCollected.findIndex(i2 => i2.path === i.path) < 0);
-                      allItemsAlreadyCollected = allItemsAlreadyCollected.concat(newItems);
-                      numberDirectoriesLeft--;
-                      if (numberDirectoriesLeft === 0) {
-                        resolve(allItemsAlreadyCollected);
-                      }
-                    })},
-                  0);
-                })
+                allItemsAlreadyCollected.push({
+                  name: filePath,
+                  path: `${path}/${filePath}`,
+                  driveName: drive.name,
+                });
+              }
+              numberFilesLeft--;
+    
+              if (numberFilesLeft === 0) {
+                if (directories.length === 0) {
+                  resolve(allItemsAlreadyCollected);
+                } else {
+                  let numberDirectoriesLeft = directories.length;
+                  let newItems: CloudItem[] = [];
+                  directories.forEach(directory => {
+                    setTimeout(() => {
+                      this.updateDriveIndex(drive, `${path}/${directory}`, allItemsAlreadyCollected).then(items => {
+                        newItems = items.filter(i => allItemsAlreadyCollected.findIndex(i2 => i2.path === i.path) < 0);
+                        allItemsAlreadyCollected = allItemsAlreadyCollected.concat(newItems);
+                        numberDirectoriesLeft--;
+                        if (numberDirectoriesLeft === 0) {
+                          resolve(allItemsAlreadyCollected);
+                        }
+                      })},
+                    0);
+                  })
+                }
               }
             }
+          } catch (e) {
+            console.log(e);
+            console.log(`> Crash with exception : ${path}/${filePath}`);
           }
         });
       });
