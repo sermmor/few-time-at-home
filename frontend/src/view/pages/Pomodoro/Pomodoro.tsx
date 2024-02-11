8bfe934cc4998083749cfaa1bbf3657f5e8b2f0e
Sergio Martín <sermmor@outlook.com>
2024-02-11 01:32:12 +0100

    Added Pomodoro Modes

--- frontend/src/view/pages/Pomodoro/Pomodoro.tsx
+++ frontend/src/view/pages/Pomodoro/Pomodoro.tsx
@@ -1,6 +1,11 @@
 import React from "react";
-import { Box, Button, SxProps, Theme, Typography } from "@mui/material";
+import { Box, Button, MenuItem, Select, SxProps, Theme, Typography } from "@mui/material";
 import { TemporalData } from "../../../service/temporalData.service";
+
+interface TimerMode {
+  name: string;
+  chain: string[]; // If 0, then use countdown field content.
+}
 
 const formStyle: SxProps<Theme> = {
   display: 'flex',
@@ -10,6 +15,15 @@ const formStyle: SxProps<Theme> = {
   justifyContent: 'center',
   fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
 };
+
+const rowFormStyle = (): SxProps<Theme> => ({
+  display: 'flex',
+  flexDirection: {xs: 'column', sm:'row'},
+  gap: '2rem',
+  alignItems: 'center',
+  justifyContent: 'center',
+  minWidth: {xs: '15.5rem', sm: '27rem', md: '50rem'},
+});
 
 const alarmPath = `${process.env.PUBLIC_URL}/alarm.mp3`;
 
@@ -41,6 +55,55 @@ const countDownTime = () => {
     setTimeDispachers.setTimeToShow!(`${formatToTwoDigits(TemporalData.TimeLeftPomodoro.minutes)}:${formatToTwoDigits(TemporalData.TimeLeftPomodoro.seconds)}`);
     setTimeout(()=> countDownTime(), oneSecond);
   }
+};
+
+const formatTimeChain = ([hours, minutes, seconds]: string[]) => `${Math.round((+hours) * 60 + (+minutes) + (+seconds) / 60)}`;
+
+const showTimerChain = (modeList: TimerMode[], currentMode: string, currentTime: string): string => {
+  let printed = '';
+  const modeIndex = modeList.findIndex(({name}) => name === currentMode);
+  modeList[modeIndex].chain.forEach((chainItem, i) => {
+    if (i === 0) {
+      printed = (chainItem !== '0') ? `${formatTimeChain(chainItem.split(':'))}` : '0';
+    } else {
+      printed = `${printed} + ${formatTimeChain(chainItem.split(':'))}`;
+    }
+  });
+  return printed === '0' ? formatTimeChain((`00:${currentTime}`).split(':')) : printed;
+};
+
+const listBaseMode: TimerMode[] = [
+  {
+    name: 'One Countdown',
+    chain: ['0'], // If 0, then use countdown field content.
+  },
+  {
+    name: 'Pomodoro',
+    chain: [
+      '00:25:00', '00:05:00',
+      '00:25:00', '00:05:00',
+      '00:25:00', '00:05:00',
+      '00:25:00', '00:05:00',
+    ],
+  },
+  // {
+  //   name: 'Test',
+  //   chain: [
+  //     '00:00:25', '00:00:05',
+  //     '00:00:25', '00:00:05',
+  //     '00:00:25', '00:00:05',
+  //     '00:00:25', '00:00:05',
+  //   ],
+  // }
+];
+
+const getCurrentChainFromModeName = (modeList: TimerMode[], modeName: string): string[] => modeList[modeList.findIndex(({name}) => name === modeName)].chain;
+
+const getCurrentChainItem = (currentChain: string[], index: number, currentTime: string): string => currentChain[index] === '0' ? currentTime : currentChain[index];
+
+const parseFromTimeFieldToTimeToShow = (timeField: string): string => {
+  const timeValueSplitted = timeField.split(':');
+  return `${formatToTwoDigits((+timeValueSplitted[0] * 60) + +timeValueSplitted[1])}:${timeValueSplitted[2]}`;
 }
 
 export const Pomodoro = (): JSX.Element => {
@@ -48,17 +111,59 @@ export const Pomodoro = (): JSX.Element => {
   const [isTimeRunning, setTimeRunning] = React.useState<boolean>(false);
   const [time, setTime] = React.useState<string>(TemporalData.getFormatTimeLeftPomodoro());
   const [timeToShow, setTimeToShow] = React.useState<string>(TemporalData.getFormatTimeLeftPomodoro());
+  const [currentMode, setCurrentMode] = React.useState<string>(listBaseMode[0].name);
+  const [currentChain, setCurrentChain] = React.useState<string[]>(listBaseMode[0].chain);
+  const [currentChainIndex, setCurrentChainIndex] = React.useState<number>(0);
+
+  const runTimer = (chainIndex: number) => {
+    const realTime = getCurrentChainItem(currentChain, chainIndex, time);
+    const realTimeToShow = parseFromTimeFieldToTimeToShow(realTime);
+    setTime(realTime);
+    setTimeToShow(realTimeToShow);
+    setTimeRunning(true);
+    const splitedTime = realTimeToShow.split(':');
+    TemporalData.TimeLeftPomodoro.minutes = +splitedTime[0];
+    TemporalData.TimeLeftPomodoro.seconds = +splitedTime[1];
+    countDownTime();
+  }
 
   setTimeDispachers.setTimeRunning = setTimeRunning;
   setTimeDispachers.setTimeToShow = setTimeToShow;
   setTimeDispachers.onFinishedCountDown = () => {
-    const timeValueSplitted = time.split(':');
-    const timeValue = `${formatToTwoDigits((+timeValueSplitted[0] * 60) + +timeValueSplitted[1])}:${timeValueSplitted[2]}`;
-    setTimeToShow(timeValue);
+    let nextChainIndex = 0;
+    let nextTime = time;
+    if (currentChainIndex + 1 >= currentChain.length) {
+      nextChainIndex = 0;
+      nextTime = getCurrentChainItem(currentChain, nextChainIndex, time);
+      setCurrentChainIndex(nextChainIndex);
+    } else {
+      nextChainIndex = currentChainIndex + 1;
+      nextTime = getCurrentChainItem(currentChain, nextChainIndex, time);
+      setCurrentChainIndex(nextChainIndex);
+    }
+    if (nextChainIndex > 0) {
+      runTimer(nextChainIndex);
+    } else {
+      setTime(nextTime);
+      setTimeToShow(parseFromTimeFieldToTimeToShow(nextTime));
+    }
   };
 
   return <Box sx={formStyle}>
-    <Box sx={{display: 'flex', flexDirection: {xs: 'column', sm:'row'}, gap: '2rem', alignItems: 'center', justifyContent: 'center', minWidth: {xs: '15.5rem', sm: '27rem', md: '50rem'}}}>
+    <Box sx={{...rowFormStyle(), justifyContent: 'left', marginLeft: '17.75rem'}}>
+      <Typography variant='h6' sx={{textTransform: 'uppercase'}}>Mode:</Typography>
+      <Select
+        value={currentMode}
+        onChange={evt => { setCurrentMode(evt.target.value); setCurrentChainIndex(0); setCurrentChain(getCurrentChainFromModeName(listBaseMode, evt.target.value)) }}
+        sx={{minWidth: '15.5rem'}}
+      >
+        {
+          listBaseMode.map(({ name: nameMode }) => <MenuItem value={nameMode} key={nameMode} sx={{textTransform: 'uppercase'}}>{nameMode.toUpperCase()}</MenuItem>)
+        }
+      </Select>
+      <Typography variant='h6' sx={{textTransform: 'uppercase'}}>{showTimerChain(listBaseMode, currentMode, time)}</Typography>
+    </Box>
+    <Box sx={rowFormStyle()}>
       <Typography variant='h6' sx={{textTransform: 'uppercase'}}>Time to countdown:</Typography>
       <input
         id="appt-time"
@@ -72,10 +177,8 @@ export const Pomodoro = (): JSX.Element => {
         value={time}
         disabled={isTimeRunning}
         onChange={evt => {
-          const timeValueSplitted = evt.target.value.split(':');
-          const timeValue = `${formatToTwoDigits((+timeValueSplitted[0] * 60) + +timeValueSplitted[1])}:${timeValueSplitted[2]}`;
           setTime(evt.target.value);
-          setTimeToShow(timeValue);
+          setTimeToShow(parseFromTimeFieldToTimeToShow(evt.target.value));
         }}
       />
       <Button
@@ -83,11 +186,7 @@ export const Pomodoro = (): JSX.Element => {
         sx={{minWidth: '7rem'}}
         onClick={() => {
           if (!isTimeRunning) {
-            setTimeRunning(true);
-            const splitedTime = timeToShow.split(':');
-            TemporalData.TimeLeftPomodoro.minutes = +splitedTime[0];
-            TemporalData.TimeLeftPomodoro.seconds = +splitedTime[1];
-            countDownTime();
+            runTimer(currentChainIndex);
           } else {
             setTimeRunning(false);
             TemporalData.TimeLeftPomodoro.minutes = 0;
