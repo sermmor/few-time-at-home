9e34cc6e9cfe8c1d666f77ea0b385f11df3128cb
Sergio Martín <sermmor@outlook.com>
2024-03-09 00:06:01 +0100

    Fix some RSS without links.

--- backend/src/blogRSS/blogRSSWorker.ts
+++ backend/src/blogRSS/blogRSSWorker.ts
@@ -1,4 +1,4 @@
-import { extract, ReaderOptions } from "@extractus/feed-extractor";
+import { extract, FeedData, ReaderOptions } from "@extractus/feed-extractor";
 import { ChannelMediaRSSMessage, ChannelMediaRSSWorkerData, cleanLinksInMessage, filterRSSMessages, updateRSSListOneByOne } from "../channelMediaRSS";
 import { parseFromBlogDateStringToDateObject, parseFromNitterDateStringToDateObject } from "../utils";
 import { WorkerChild } from "../workerModule/workersManager";
@@ -8,15 +8,38 @@ const updateRSS = (
     data: ChannelMediaRSSWorkerData,
     endpoint: string,
     nitterUrlIndex: number = 0,
-    currentTry = 4
+    currentTry = 4,
+    rssOptionsAlternative?: ReaderOptions,
+    feedData?: FeedData,
 ): Promise<ChannelMediaRSSMessage[]> => {
-    const { rssOptions } = <BlogRSSWorkerData> data;
+    const { rssOptions } = rssOptionsAlternative ? { rssOptions: rssOptionsAlternative, } : <BlogRSSWorkerData> data;
     return new Promise<ChannelMediaRSSMessage[]>(resolve =>
-        extract(`${endpoint}`, rssOptions).then((data) => {
+        extract(`${endpoint}`, rssOptions).then((dataItem) => {
             // console.log(data)
-            const currentMessages: ChannelMediaRSSMessage[] = mapRSSBlogPostsToMessages(data);
-            console.log(`${endpoint}  ${currentMessages.length}`);
-            resolve(currentMessages);
+            if (dataItem && dataItem.entries && dataItem.entries[0] && dataItem.entries[0].link === '') {
+              // When RSS hasn't link, then try with NO normalization RSS.
+              const newRssOptions = {
+                ...rssOptions,
+                normalization: false,
+              };
+              setTimeout(() => updateRSS(data, endpoint, nitterUrlIndex, currentTry, newRssOptions, dataItem).then(data => resolve(data)), 100);
+            } else {
+              let currentMessages: ChannelMediaRSSMessage[];
+              if (feedData) {
+                currentMessages = mapRSSBlogPostsToMessages(feedData);
+                if (!!dataItem && !!(<any> dataItem).item[0] && !!(<any> dataItem).item[0].enclosure && !!(<any> dataItem).item[0].enclosure['@_url']) {
+                  // Get links Prisa ES from NO normalization RSS.
+                  currentMessages = currentMessages.map((message: any, index: number) => ({
+                    ...message,
+                    originalLink: (<any> dataItem).item[index].enclosure['@_url'],
+                  }));
+                }
+              } else {
+                currentMessages = mapRSSBlogPostsToMessages(dataItem);
+              }
+              console.log(`${endpoint}  ${currentMessages.length}`);
+              resolve(currentMessages);
+            }
         }).catch((err) => {
           if ((`${err}`).indexOf('error code 504') > -1) {
             console.log('Error: Request failed with error code 504');
