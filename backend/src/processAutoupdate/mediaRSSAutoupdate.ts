aaf8bc6b2817739c7f7544ed4b91f43b13e46e21
Sergio Martín <sermmor@outlook.com>
2025-06-24 13:49:46 +0200

    Fixing problem with duplicates

--- backend/src/processAutoupdate/mediaRSSAutoupdate.ts
+++ backend/src/processAutoupdate/mediaRSSAutoupdate.ts
@@ -126,7 +126,7 @@ export class MediaRSSAutoupdate {
       return messages;
     }
     
-    const messagesFixed = messages.filter(message => message && message !== '' && message.split('\n').length >= 1).reverse();
+    const messagesFixed = messages.filter(message => message && message !== '' && message.split('\n').length >= 1);
 
     const messagesWithoutDuplicates: string[] = [];
 
@@ -137,7 +137,7 @@ export class MediaRSSAutoupdate {
       }
     }
 
-    return messagesWithoutDuplicates.reverse();
+    return messagesWithoutDuplicates;
   }
 
   private saveYoutubeFavorites = async (): Promise<boolean> => {
